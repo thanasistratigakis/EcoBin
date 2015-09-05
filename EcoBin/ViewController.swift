bbab882a9d6095ed013d60f15ceef3356420239e
ThanasiStratigakis <thanasi.strati@gmail.com>
2015-09-05 03:12:20 -0400

    Initial Commit

---
+++ EcoBin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EcoBin
+//
+//  Created by Thanasi Stratigakis on 9/5/15.
+//  Copyright (c) 2015 Thanasi Stratigakis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
