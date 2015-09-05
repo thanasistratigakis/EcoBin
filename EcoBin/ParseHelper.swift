ba183958e4150e1fd27625d8f0b1be874238196c
ThanasiStratigakis <thanasi.strati@gmail.com>
2015-09-05 13:07:23 -0400

    leaderboardUsercell files created

---
+++ EcoBin/ParseHelper.swift
@@ -0,0 +1,9 @@
+//
+//  ParseHelper.swift
+//  EcoBin
+//
+//  Created by Thanasi Stratigakis on 9/5/15.
+//  Copyright (c) 2015 Thanasi Stratigakis. All rights reserved.
+//
+
+import Foundation
---
+++ EcoBin/ViewControllers/LeaderboardCells/LeaderboardUserCell.swift
@@ -0,0 +1,30 @@
+//
+//  LeaderboardUserCell.swift
+//  EcoBin
+//
+//  Created by Thanasi Stratigakis on 9/5/15.
+//  Copyright (c) 2015 Thanasi Stratigakis. All rights reserved.
+//
+
+import UIKit
+
+class LeaderboardUserCell: UITableViewCell {
+    
+    // Outlets go here
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
