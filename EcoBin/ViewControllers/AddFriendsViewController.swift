5596a53a375351461b3c9693c6ab33a50933f47b
ThanasiStratigakis <thanasi.strati@gmail.com>
2015-09-05 10:09:41 -0400

    ViewController classes created

---
+++ EcoBin/ViewControllers/AddFriendsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddFriendsViewController.swift
+//  EcoBin
+//
+//  Created by Thanasi Stratigakis on 9/5/15.
+//  Copyright (c) 2015 Thanasi Stratigakis. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ EcoBin/ViewControllers/MyAccountViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MyAccountViewController.swift
+//  EcoBin
+//
+//  Created by Thanasi Stratigakis on 9/5/15.
+//  Copyright (c) 2015 Thanasi Stratigakis. All rights reserved.
+//
+
+import UIKit
+
+class MyAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
